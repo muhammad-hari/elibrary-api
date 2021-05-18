5e6fffd3a879dcbdb8283db6860b02e934be8d0c
Muhammad Hari <muhammad.hari@syntechmi.com>
2021-05-18 21:26:43 +0700

    chore(feat): add customer & payment controller

---
+++ Moonlay.ELibrary.Api/Microservices/Api/Moonlay.ELibrary.Api/Controllers/CustomersController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Moonlay.ELibrary.Application.Interfaces;
+using Moonlay.ELibrary.Application.Models;
+using Moonlay.ELibrary.Domain.Interfaces;
+
+namespace Moonlay.ELibrary.Api.Controllers
+{
+    [ApiController]
+    [Route("api/v1/[controller]")]
+    public class CustomersController : ControllerBase
+    {
+        #region Private Members
+
+        private readonly IRentBookService bookService;
+        private readonly ILibraryRepository repository;
+        private readonly ILogger<CustomersController> logger;
+
+        #endregion
+
+        #region Constructor
+
+        public CustomersController(ILogger<CustomersController> logger, IRentBookService bookService, ILibraryRepository repository)
+        {
+            this.logger = logger;
+            this.bookService = bookService;
+            this.repository = repository;
+        }
+
+        #endregion
+
+        [HttpGet, Route("{id}")]
+        public ActionResult Get(int id)
+        {
+            var cust = repository.GetCustomer(id);
+            return Ok(cust);
+        }
+
+        [HttpPost]
+        public ActionResult Add([FromBody] CustomerDto customerDto)
+        {
+            repository.AddCustomer(new Domain.Models.Customer()
+            {
+               FirstName = customerDto.FirstName,
+               LastName = customerDto.LastName,
+               Gender = customerDto.Gender ?? "N",
+               Phone = customerDto.Phone,
+            });
+
+            return Ok(customerDto);
+        }
+
+
+    }
+}
---
+++ Moonlay.ELibrary.Api/Microservices/Api/Moonlay.ELibrary.Api/Controllers/PaymentController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Moonlay.ELibrary.Application.Interfaces;
+using Moonlay.ELibrary.Application.Models;
+using Moonlay.ELibrary.Application.Models.Request;
+using Moonlay.ELibrary.Domain.Interfaces;
+
+namespace Moonlay.ELibrary.Api.Controllers
+{
+    [ApiController]
+    [Route("api/v1/[controller]")]
+    public class PaymentController : ControllerBase
+    {
+        #region Private Members
+
+        private readonly IRentBookService bookService;
+        private readonly ILibraryRepository repository;
+        private readonly ILogger<CustomersController> logger;
+
+        #endregion
+
+        #region Constructor
+
+        public PaymentController(ILogger<CustomersController> logger, IRentBookService bookService, ILibraryRepository repository)
+        {
+            this.logger = logger;
+            this.bookService = bookService;
+            this.repository = repository;
+        }
+
+        #endregion
+
+        [HttpGet, Route("{id}")]
+        public ActionResult Get(int id)
+        {
+            var cust = repository.GetCustomer(id);
+            return Ok(cust);
+        }
+
+        [HttpPost]
+        public ActionResult Add([FromBody] RentBookRequest request)
+        {
+            var response = bookService.PaymentAsync(request);
+
+            return Ok(response.Result);
+        }
+
+
+    }
+}
